f525ed6e02f7115dca708aa858e61821ebdf72cb
Lucas <45589886+MrSoxis@users.noreply.github.com>
2019-02-24 20:21:56 +0100

    Update lginfo.js

--- commands/lginfo.js
+++ commands/lginfo.js
@@ -33,7 +33,7 @@ module.exports.run=async(bot,message,args)=>{
                         .addField("Top : "+hameau.currentRank, "Points : "+hameau.points)
                         .setThumbnail("https://www.loups-garous-en-ligne.com"+hameau.picture)
                         .addField("Nombre de membres", hameau.membersCount);
-                    message.channel.send("https://www.loups-garous-en-ligne.com"+hameau.picture);
+                    message.channel.send("",{file["https://www.loups-garous-en-ligne.com"+hameau.picture]});
                 }
 
 
