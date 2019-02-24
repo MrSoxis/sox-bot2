0f4e86c589b8ed981dd126d327d13a6adfd8c1e7
Lucas <45589886+MrSoxis@users.noreply.github.com>
2019-02-24 13:19:31 +0100

    Update lginfo.js

--- commands/lginfo.js
+++ commands/lginfo.js
@@ -27,7 +27,7 @@ module.exports.run=async(bot,message,args)=>{
                 .addField("Statut ",prem);
                 if(user.hamlet) {
                     var hameau = user.hamlet;
-                    embed.addField("Hameau ", "https://www.loups-garous-en-ligne.com"+hameau.picture)
+                    embed.addField("Hameau ", "--------")
                         .addField("Nom :"+ hameau.name, "Tag : [" + hameau.tag + "]")
                         .addField("Role", hameau.role)
                         .addField("Top : "+hameau.currentRank, "Points : "+hameau.points)
