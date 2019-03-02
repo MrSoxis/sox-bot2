2660e092eaac9764911a26d74e400d72629aa4c2
Lucas <45589886+MrSoxis@users.noreply.github.com>
2019-03-02 17:07:13 +0100

    Update lginfo.js

--- commands/lginfo.js
+++ commands/lginfo.js
@@ -39,6 +39,9 @@ module.exports.run=async(bot,message,args)=>{
                         .addField("Top : "+hameau.currentRank, "Points : "+hameau.points)                        
                         .addField("Nombre de membres", hameau.membersCount);
                 }
+            if(user.roles[0].id){
+                embed.addField("Role",user.role[0].name);
+            }
 
 
             return message.channel.send(embed);
