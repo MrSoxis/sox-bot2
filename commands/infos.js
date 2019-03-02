8237b901a993de434c198a6dd140f7459407a1cf
Lucas <45589886+MrSoxis@users.noreply.github.com>
2019-03-02 11:17:14 +0100

    Update infos.js

--- commands/infos.js
+++ commands/infos.js
@@ -13,11 +13,10 @@ module.exports.run=async(bot,message,args)=>{
         .addField("?serveur",'Informations sur le serveur')
         .addField("?select + @User","Selectionne un utilisateur et retourne ses infos")
         .addField("?lg + pseudo lgel","Retourne des informations sur un joueur lgel")
-        .addField("?lginfo + pseudo lgel","Retourne les informations complètes d'un joueur");
-    var user = message.author;
-    user.createDM().then(channel=>{
+        .addField("?lginfo + pseudo lgel","Retourne les informations complètes d'un joueur");    
+    message.author.createDM().then(channel=>{
         return channel.send(embed);
-    }).catch(console.error)
+    }).catch(console.error);
 };
 module.exports.help={
     name:"infos"
