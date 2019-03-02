ed1bcd36014f19c6241ce232a1d61d1096f778c7
Lucas <45589886+MrSoxis@users.noreply.github.com>
2019-03-02 02:11:27 +0100

    Update lginfo.js

--- commands/lginfo.js
+++ commands/lginfo.js
@@ -2,7 +2,7 @@ const Discord=require('discord.js');
 const superagent = require("superagent");
 const snekfetch = require("snekfetch");
 module.exports.run=async(bot,message,args)=>{
-    var api="https://www.loups-garous-en-ligne.com/api/profile.php?user="+args[0]+"&fields=id,title,gender,signature,points,isPremium,hamlet,activity";
+    var api="https://www.loups-garous-en-ligne.com/api/profile.php?user="+args[0]+"&fields=id,title,gender,signature,points,isPremium,hamlet,activity,mdj,roles";
     snekfetch.get(api).then(r=>{
         var user=r.body;
         if(user.error){
