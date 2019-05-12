4a84ef60c9cb0c201c4ceb02582ebabd980d0eb8
Lucas <45589886+MrSoxis@users.noreply.github.com>
2019-05-12 18:34:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ bot.on('message',async message=>{
             return message.channel.send("Bot en maintenance, oups :$");
           }
         };
-        if(banGuild.includes(message.guild.id)){return message.channel.send("Bot indisponible sur ce serveur, raison ");};
+        if(banGuild.includes(message.guild.id)){return message.channel.send("Bot indisponible sur ce serveur cheh à vous");};
         if(banUser.includes(message.author.id)){ message.react('🖕'); return message.author.createDM().then(channel=>{
             return channel.send("Mdr t ban");
             }).catch(console.error);};
