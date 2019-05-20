dd3b3c29d27ad3c413cd780a731d601218af9be4
Lucas <45589886+MrSoxis@users.noreply.github.com>
2019-05-20 10:42:34 +0200

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -2,7 +2,7 @@ const Discord=require('discord.js');
 const superagent = require("superagent");
 const snekfetch = require("snekfetch");
 module.exports.run=async(bot,message,args)=>{
-  var api="https://www.loups-garous-en-ligne.com/api/profile.php?user="+args[0]+"&fields=id,title,gender,signature,points,isPremium,hamlet,activity,mdj,roles";
+  var api="https://aws.random.cat/meow";
     snekfetch.get(api).then(r=>{
         var data=r.body;
         return message.channel.send(data.file);
