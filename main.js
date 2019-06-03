24d5411c7f1505d0f134242a70e4a5ebfafe8231
Lucas <45589886+MrSoxis@users.noreply.github.com>
2019-06-03 10:14:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const bot = new Discord.Client({disableEveryone : true});
 const maintenance=0;
 const banGuild=[];
-const banUser=["536927971215409161","365431431251492865"];
+const banUser=["536927971215409161"];
 bot.commands = new Discord.Collection();
 
 fs.readdir('./commands/',(err,files)=>{
